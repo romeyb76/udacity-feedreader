e7c4800f61c94eabd9e2acfb04d3e5b3f980bcc9
Jerome Biggins <jeromeebiggins@gmail.com>
2018-09-13 06:46:48 -0500

    Completed tests and updated README file.

--- feedreader.js
+++ feedreader.js
@@ -98,10 +98,28 @@ $(function() {
         });
     });
     /* TODO: Write a new test suite named "New Feed Selection" */
-
+    describe('New Feed Selection', function() {
+        const feed = document.querySelector('.feed');
+        const firstFeed = [];
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+         beforeEach(function(done) {
+             loadFeed(0);
+             Array.from(feed.children).forEach(function(entry) {
+                 firstFeed.push(entry.innerText);
+             });
+             loadFeed(1, done);
+         });
+        
+         it('content changes', function() {
+             Array.from(feed.children).forEach(function(entry,index) {
+                expect(entry.innerText, firstFeed[index], entry.innerText === firstFeed[index]);
+                expect(entry.innerText === firstFeed[index]).toBe(false);
+             });
+         });
    
+});
+    
 });
